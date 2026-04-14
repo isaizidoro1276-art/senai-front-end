b46df250fe003ede53aec6b3ee2be6cbe03b1e8e
isabellaizidoro212-wq <isabellaizidoro212@gmail.com>
2026-04-14 13:03:24 -0300

    Renderizando templates, com alguns erros

--- ctrl_home.py
+++ ctrl_home.py
@@ -13,9 +13,10 @@ bp = Blueprint(__name__, "HomeController")
 @bp.route("/") # cria uma rota
 def index(): # função que gerencia rota
     """ Página inicial"""
-    # if 'user' not in session:
-    #     return redirect(url_for("auth.login"))
-    
+    if '_user_id' not in session:
+        
+        return redirect(url_for("auth.login"))
+
     return render_template("dashboard/index.html") # Renderiza um template
 
 @bp.route("/dashboard")  # cria uma rota
